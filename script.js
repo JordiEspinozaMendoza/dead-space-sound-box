3d9b86b2ceece54fbfbc7ab6e89ef1b6b544cd7a
Jordi <jordi8101@gmail.com>
2025-10-27 21:17:45 -0700

    Add quarantine alarm and make us whole again sound effects with corresponding buttons

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const plasmaCutterSound = document.getElementById("plasma-cutter-sfx");
 const twinkleSound = document.getElementById("twinkle-sfx");
 const necromorphSounds = document.querySelectorAll(".necromorph-sfx");
-
+const quarantineAlarmSound = document.getElementById("quarantine-alarm-sfx");
+const makeUsWholeAgainSound = document.getElementById("make-us-whole-again-sfx");
 const plasmaCutterButton = document.getElementById("plasma-cutter-sfx-button");
 const necromorphButton = document.getElementById("necromorph-sfx-button");
 const twinkleButton = document.getElementById("twinkle-sfx-button");
+const quarantineAlarmButton = document.getElementById("quarantine-alarm-button");
+const makeUsWholeAgainButton = document.getElementById("make-us-whole-again-button");
 
 plasmaCutterButton.addEventListener("click", function () {
   playPlasmaCutterSound();
@@ -12,6 +15,14 @@ plasmaCutterButton.addEventListener("click", function () {
 
 twinkleButton.addEventListener("click", function () {
   playTwinkleSound();
+});
+
+quarantineAlarmButton.addEventListener("click", function () {
+  playQuarantineAlarmSound();
+});
+
+makeUsWholeAgainButton.addEventListener("click", function () {
+  playMakeUsWholeAgainSound();
 });
 
 let lastNecromorphIndex = null;
@@ -24,6 +35,16 @@ necromorphButton.addEventListener("click", function () {
 const playPlasmaCutterSound = () => {
   plasmaCutterSound.currentTime = 0;
   plasmaCutterSound.play();
+};
+
+const playQuarantineAlarmSound = () => {
+  quarantineAlarmSound.currentTime = 0;
+  quarantineAlarmSound.play();
+}
+
+const playMakeUsWholeAgainSound = () => {
+  makeUsWholeAgainSound.currentTime = 0;
+  makeUsWholeAgainSound.play();
 };
 
 let lastPlayed = 0;
@@ -50,10 +71,14 @@ const playTwinkleSound = () => {
   twinkleSound.play();
 };
 
-window.addEventListener("gamepadconnected", function(e) {
+window.addEventListener("gamepadconnected", function (e) {
   const gamepad = e.gamepad;
   console.log(`Gamepad connected at index ${gamepad.index}: ${gamepad.id}.
               ${gamepad.buttons.length} buttons, ${gamepad.axes.length} axes.`);
+  document.getElementById(
+    "gamepad-status"
+  ).textContent = `Gamepad connected: ${gamepad.id}`;
+  loop();
 });
 
 window.addEventListener("gamepaddisconnected", (e) => {
@@ -62,14 +87,12 @@ window.addEventListener("gamepaddisconnected", (e) => {
 
 function loop() {
   const gamepads = navigator.getGamepads();
-
   for (const gamepad of gamepads) {
     if (!gamepad) continue;
 
     gamepad.buttons.forEach((btn, i) => {
       if (btn.pressed) {
-        console.log(i);
-
+        console.log(`Button ${i} pressed`);
         switch (i) {
           case 7: // ZR Button
             playPlasmaCutterSound();
@@ -82,6 +105,14 @@ function loop() {
             break;
           case 3: // Y Button
             playTwinkleSound();
+            break;
+          case 2: // B Button
+            playQuarantineAlarmSound();
+            break;
+          case 0: // A Button
+            playMakeUsWholeAgainSound();
+            break;
+          default:
             break;
         }
       }
